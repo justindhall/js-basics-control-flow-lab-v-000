2cf04563cb4f17546b3faa4b0f751b6664401d90
Justin Hall <hall.justin.d@gmail.com>
2018-05-13 02:08:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,7 +30,10 @@ function switchOnCharmFromTip(tip){
   switch(tip){
     case "generous"
       return "Thank you so much."
-    
+    case "not as generous"
+      return "Thank you."
+    default
+      return "Bye."
   
   }
   
